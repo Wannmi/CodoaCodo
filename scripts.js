ef270d522c092554b2dc13026bf6c8dcc61d4853
Wannmi <juan.mascaro@alu.ing.unlp.edu.ar>
2023-04-22 00:38:59 -0300

    Menu hamburguesa movil

---
+++ scripts.js
@@ -0,0 +1,22 @@
+/* Menu hamburguesa */
+/* Abre el menu */
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+/* cierra el menu */
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+/* fin del Menu hamburguesa */
